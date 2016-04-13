dad44faf25786447881b1c02c53fc053c1cd2c36
Jack88 <maxwellfrancis@hotmail.com>
2016-04-13 12:09:10 +0200

    creation de PreferenceformatDao.java

--- src/publication_workspace/chimn/src/main/java/ensg/tsig/chimn/dao/PreferenceformatDao.java
+++ src/publication_workspace/chimn/src/main/java/ensg/tsig/chimn/dao/PreferenceformatDao.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import ensg.tsig.chimn.entities.MetaData;
 import ensg.tsig.chimn.entities.Preferenceformat;
 
-/**
- * @author hanane
- *
- */
+
 public interface PreferenceformatDao extends CrudRepository<Preferenceformat, Long>{
-	//public List<Preferenceformat> findAll();
+
+	public List<Preferenceformat> findById(Long id);
+	 public List<Preferenceformat> findByNom(String nom);
+	 public List<Preferenceformat> findByActivated(boolean activated);
+	
 
 }
