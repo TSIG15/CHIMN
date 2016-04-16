e342b32f5a83db7a9e9e83966d60a856c78a2889
Nisrine Drissi <nisrine.drissi@gmail.com>
2016-04-16 16:17:09 +0200

    comments for upload page

--- src/interfaceAdministration_workspace_OLD/upload_page_user.php
+++ src/interfaceAdministration_workspace_OLD/upload_page_user.php
@@ -3,6 +3,62 @@
     include ('includes/test_File_requeteBDDupload.php');
     ?>
 
+<!-- JAVASCRIPT : Empeche la selection de plusieurs formats OU plusieurs SRS / oblige la selection de 1 format et 1 SRS -->
+
+<script language="JavaScript" type="text/javascript">
+var titledata = "testFichier";
+var tlurl = <?php echo json_encode($chemin); ?>;
+var result;
+var FormatChoice;
+var srsChoice;
+
+function cheminFinal() {
+    FormatChoice = "vide";
+    srsChoice = "vide";
+    
+    if (document.getElementById('shp').checked) {
+        var FormatChoice = 'shp';
+    }
+    else if (document.getElementById('dxf').checked) {
+        var FormatChoice = 'dxf';
+    }
+    else if (document.getElementById('gml').checked) {
+        var FormatChoice = 'gml';
+    }
+    else if (document.getElementById('kml').checked) {
+        var FormatChoice = 'kml';
+    }
+    else if (FormatChoice == "vide" && srsChoice !== "vide")
+    {
+        alert("Veuillez selectionner un format")
+    }
+    
+    if (document.getElementById('2154').checked) {
+        var srsChoice = '2154';
+    }
+    else if (document.getElementById('3857').checked) {
+        var srsChoice = '3857';
+    }
+    else if (document.getElementById('32631').checked) {
+        var srsChoice = '32631';
+    }
+    else if (srsChoice =="vide" && FormatChoice !== "vide")
+    {
+        alert("Veuillez selectionner un système de coordonnées")
+    }
+    
+    if (FormatChoice !=="vide" && srsChoice !=="vide"){
+        var result = tlurl + FormatChoice + "/" + "EPSG" + srsChoice + "/" + titledata + ".zip";
+        return result;
+    }
+    else if (FormatChoice =="vide" && srsChoice =="vide"){
+        alert("Veuillez selectionner un format et un système de coordonnées")
+    }
+}
+</script>
+
+
+<!--- HTML : PAGE DE TELECHARGEMENT DE DONNEES POUR L UTILISATEUR : Choix du format et du SRS -->
 <div class="container-fluid">
 <div class="row">
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main index">
@@ -37,60 +93,6 @@
 </div>
 </br>
 
-<script language="JavaScript" type="text/javascript">
-var titledata = "testFichier";
-var tlurl = <?php echo json_encode($chemin); ?>;
-var result;
-var FormatChoice;
-var srsChoice;
-
-function cheminFinal() {
-    FormatChoice = "vide";
-    srsChoice = "vide";
-    
-    if (document.getElementById('shp').checked) {
-        var FormatChoice = 'shp';
-    }
-    else if (document.getElementById('dxf').checked) {
-        var FormatChoice = 'dxf';
-    }
-    else if (document.getElementById('gml').checked) {
-        var FormatChoice = 'gml';
-    }
-    else if (document.getElementById('kml').checked) {
-        var FormatChoice = 'kml';
-    }
-    else if (FormatChoice == "vide" && srsChoice !== "vide")
-    {
-        alert("Veuillez selectionner un format")
-    }
-
-
-    if (document.getElementById('2154').checked) {
-        var srsChoice = '2154';
-    }
-    else if (document.getElementById('3857').checked) {
-        var srsChoice = '3857';
-    }
-    else if (document.getElementById('32631').checked) {
-        var srsChoice = '32631';
-    }
-    else if (srsChoice =="vide" && FormatChoice !== "vide")
-    {
-        alert("Veuillez selectionner un système de coordonnées")
-    }
-    
-    if (FormatChoice !=="vide" && srsChoice !=="vide"){
-        var result = tlurl + FormatChoice + "/" + "EPSG" + srsChoice + "/" + titledata + ".zip";
-        return result;
-    }
-    else if (FormatChoice =="vide" && srsChoice =="vide"){
-        alert("Veuillez selectionner un format et un système de coordonnées")
-    }
-}
-
-</script>
-
 <a  button type ="button"
     class="btn btn-primary btn-block"
     id "button"
@@ -98,6 +100,9 @@ function cheminFinal() {
     href="javascript:window.location=cheminFinal();" >TÉLÉCHARGER</a>
 
 <!--fin téléversements-->
+
+
+
 
 
 </div>
